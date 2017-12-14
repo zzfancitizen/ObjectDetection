9526dbe65682d8f894809baf0c12a3d26c3dbbd0
zzfancitizen <zzfancitizen@gmail.com>
2017-12-14 18:24:12 +0800

    push 3rd.py

--- test/3rd.py
+++ test/3rd.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
     matrix = np.reshape(np.asarray([0, 1, 2, 3, 4, 5, 6, 7]), (4, 2))
     argmax_x = matrix.argmax(axis=1)
 
+    print(argmax_x)
+
     max = matrix[np.arange(4), argmax_x]
     print(matrix)
     print(max)
