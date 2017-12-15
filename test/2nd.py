4261c64c47be0a6a4944301c75c4103b5ff7f6de
zzfancitizen <zzfancitizen@gmail.com>
2017-12-15 14:08:32 +0800

    push 3rd.py

--- test/2nd.py
+++ test/2nd.py
@@ -1,16 +1,29 @@
 import tensorflow as tf
 
+# _I = 0
+
 if __name__ == '__main__':
-    a = tf.constant(1.)
-    b = tf.constant(1.)
-    b = 2 * a
+    # deltas = [0.1, 0, 0.3]
+    # sigma2 = 1
+    # deltas_abs = tf.abs(deltas)
+    # smoothL1_sign = tf.cast(tf.less(deltas_abs, 1.0 / sigma2), tf.float32)
+    # x = tf.reduce_sum(tf.square(deltas) * 0.5 * sigma2 * smoothL1_sign + \
+    #                   (deltas_abs - 0.5 / sigma2) * tf.abs(smoothL1_sign - 1))
+    # with tf.Session() as sess:
+    #     while _I < 10000:
+    #         print(sess.run(x))
+    #         _I += 1
 
-    g = tf.gradients(a + b, [a, b])[0]
+    a = tf.constant(3., dtype=tf.float32)
+    b = tf.constant(2., dtype=tf.float32)
 
-    print(g)
+    flag = tf.less(a, b)
 
-    init = tf.global_variables_initializer()
+    init = tf.group(tf.local_variables_initializer(), tf.global_variables_initializer())
 
     with tf.Session() as sess:
         sess.run(init)
-        print(sess.run(g))
+
+        print(sess.run(flag))
+        print(tf.__version__)
+
